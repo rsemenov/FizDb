a75bb8e37fcb0e0d3ee88585bfc7103c93dcb038
Ruslan Semenov <ruslan@rsemenov.com>
2012-10-16 20:05:23 +0300

    add script to create users table

---
+++ create_sportdb_userstable.sql
@@ -0,0 +1,24 @@
+USE [Sportdb]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 10/16/2012 20:04:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Login] [nvarchar](50) NOT NULL,
+	[Pass] [nvarchar](50) NOT NULL,
+	[UserType] [int] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
